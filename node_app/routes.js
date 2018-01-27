db841ca0dc814ba1a49144219c07d962a29cff86
kaitlynng <kaitlyn.nky@gmail.com>
2018-01-28 03:44:49 +0800

    edited html file for form data

--- node_app/routes.js
+++ node_app/routes.js
@@ -1,12 +1,11 @@
 var db = require('./database');
-var pythonComm = require('python-comm');
-
+var pythonComm = require('./python-comm');
 
 module.exports = function(app) {
   app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/main.html');
   });
-  app.POST('/brew', function(req, res) {
+  app.post('/brew', function(req, res) {
     console.log(JSON.stringify(req.body))
   });
 };
